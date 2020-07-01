5e5a9ab5900e709c16be0db5796ef3c42a49448d
Bima Saridjo <palessie27@gmail.com>
2020-07-01 18:56:41 -0400

    push workshop

--- models/index.js
+++ models/index.js
@@ -2,4 +2,18 @@ const Person = require('./person');
 const Recipe = require('./recipe');
 //YOUR CODE HERE
 
+Person.hasMany(Recipe);
+Recipe.belongsTo(Person);
+
+Recipe.findWithRecipes = function(){
+    return Person.findAll({
+        include: Recipe
+    })
+}
+
+Person.prototype.writeRecipe = function(obj){
+    return Recipe.create({
+    })
+}
+
 module.exports = { Person, Recipe };
